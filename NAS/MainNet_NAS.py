06b96482aa40469ae03aeb9f9c4dbd24fe8ea3a5
R0n12 <xulang7@gmail.com>
2021-08-02 13:22:04 -0400

    added MixModule prototype (IN PROGRESS)

--- NAS/MainNet_NAS.py
+++ NAS/MainNet_NAS.py
@@ -77,10 +77,11 @@ class CSFI2(nn.Module):
 class CSFI2Module(nn.Module):
     def __init__(self, num_res_blocks, n_feats, res_scale):
         super(CSFI2Module, self).__init__()
+        self.num_res_blocks = num_res_blocks
         self.ex12 = CSFI2(n_feats)
         self.RB21 = nn.ModuleList()
         self.RB22 = nn.ModuleList()
-        for i in range(self.num_res_blocks[2]):
+        for i in range(self.num_res_blocks):
             self.RB21.append(ResBlock(in_channels=n_feats, out_channels=n_feats,
                 res_scale=res_scale))
             self.RB22.append(ResBlock(in_channels=n_feats, out_channels=n_feats,
@@ -94,7 +95,7 @@ class CSFI2Module(nn.Module):
         ## cell 1-8 and 2-2
         x21_res, x22_res = self.ex12(x21_res, x22_res)
         ## cell 1-10 2-4
-        for i in range(self.num_res_blocks[2]):
+        for i in range(self.num_res_blocks):
             x21_res = self.RB21[i](x21_res)
             x22_res = self.RB22[i](x22_res)
         ## 1-11 2-5
@@ -147,12 +148,12 @@ class CSFI3(nn.Module):
 class CSFI3Module(nn.Module):
     def __init__(self, num_res_blocks, n_feats, res_scale):
         super(CSFI3Module, self).__init__()
+        self.num_res_blocks = num_res_blocks
         self.ex123 = CSFI3(n_feats)
-
         self.RB31 = nn.ModuleList()
         self.RB32 = nn.ModuleList()
         self.RB33 = nn.ModuleList()
-        for i in range(self.num_res_blocks[3]):
+        for i in range(self.num_res_blocks):
             self.RB31.append(ResBlock(in_channels=n_feats, out_channels=n_feats,
                 res_scale=res_scale))
             self.RB32.append(ResBlock(in_channels=n_feats, out_channels=n_feats,
@@ -169,7 +170,7 @@ class CSFI3Module(nn.Module):
         ## 1-13 2-7 4-2
         x31_res, x32_res, x33_res = self.ex123(x31_res, x32_res, x33_res)
         ## 1-15 2-9 4-4
-        for i in range(self.num_res_blocks[3]):
+        for i in range(self.num_res_blocks):
             x31_res = self.RB31[i](x31_res)
             x32_res = self.RB32[i](x32_res)
             x33_res = self.RB33[i](x33_res)
@@ -201,14 +202,15 @@ class UpScaleModule(nn.Module):
 class SAModule1x(nn.Module):
     def __init__(self, num_res_blocks, n_feats, res_scale):
         super(SAModule1x, self).__init__()
+        self.num_res_blocks = num_res_blocks
         self.conv11_head = conv3x3(256+n_feats, n_feats)
         self.RB11 = nn.ModuleList()
-        for i in range(num_res_blocks[1]):
+        for i in range(self.num_res_blocks):
             self.RB11.append(ResBlock(in_channels=n_feats, out_channels=n_feats,
                 res_scale=res_scale))
         self.conv11_tail = conv3x3(n_feats, n_feats)
     
-    def forward(self, num_res_blocks, x, T_lv3, S):
+    def forward(self, x, T_lv3, S):
         x11 = x
         ### soft-attention
         x11_res = x11
@@ -220,23 +222,27 @@ class SAModule1x(nn.Module):
 
         x11_res = x11
         ## 16 RB at cell 1-5
-        for i in range(num_res_blocks[1]):
+        for i in range(self.num_res_blocks):
             x11_res = self.RB11[i](x11_res)
         x11_res = self.conv11_tail(x11_res)
         
         ## cell 1-7
         x11 = x11 + x11_res
 
-        return x11
+        x21 = x11
+        x21_res = x21
+
+        return x11, x21, x21_res
 
 class SAModule2x(nn.Module):
-    def __init__(self, num_res_blocks, n_feats, res_scale):
+    def __init__(self, num_res_blocks, n_feats):
         super(SAModule1x, self).__init__()
+        self.num_res_blocks = num_res_blocks
         ### stage21, 22 - CSFI2 Module: Info exchange between 1x res & 2x res
         #self.conv21_head = conv3x3(n_feats, n_feats)
         self.conv22_head = conv3x3(128+n_feats, n_feats)
     
-    def forward(self, num_res_blocks, x22, T_lv2, S):
+    def forward(self, x22, T_lv2, S):
         ### soft-attention
         ## TT at cell 2-1
         x22_res = x22
@@ -268,6 +274,31 @@ class SAModule3x(nn.Module):
 
         return x33
 
+class MixModule(nn.Module):
+    def __init__(self, num_res_blocks, n_feats, res_scale, stage_num):
+        super(MixModule, self).__init__()
+        self.num_res_blocks = num_res_blocks
+        if stage_num == 2:
+            self.SAModule2x = SAModule2x(self.num_res_blocks, n_feats)
+            self.CSFI2Module = CSFI2Module(self.num_res_blocks, n_feats, res_scale)
+        if stage_num == 3:
+            self.SAModule3x = SAModule3x(self.num_res_blocks, n_feats)
+            self.CSFI3Module = CSFI3Module(self.num_res_blocks, n_feats, res_scale)
+        self.softmax = nn.Softmax(dim=0)
+
+    def forward(self, x, weights, stage_num):
+        if stage_num == 2:
+            self.SAModule2x()
+        h_x = self.horizontal(x)
+        f_x = self.fuse(x)
+
+        weights = self.softmax(weights)
+
+        x_list = []
+        for i in range(len(x)):
+            x_list.append(weights[0]*h_x[i]+weights[1]*f_x[i])
+        
+        return x_list
         
 class MergeTail(nn.Module):
     def __init__(self, n_feats):
@@ -293,62 +324,70 @@ class MergeTail(nn.Module):
 
 
 class MainNet_NAS(nn.Module):
-    def __init__(self, num_res_blocks, n_feats, res_scale):
+    def __init__(self, num_res_blocks, n_feats, res_scale, list_modules=[1,1]):
         super(MainNet_NAS, self).__init__()
         self.num_res_blocks = num_res_blocks ### a list containing number of resblocks of different stages
-        self.n_feats = n_feats
+        self.list_modules = list_modules
 
         # Soft Feature Extraction Stage for 1x feat [necessary]
         self.SFE = SFE(self.num_res_blocks[0], n_feats, res_scale)
 
         # Soft Attention Module 1x -> see forward() for actual computation
-        self.stage1x = self.__make_stage1x(num_res_blocks, n_feats, res_scale) ### make stage 1x search space
+        #self.stage1x = self.__make_stage1x(n_feats, res_scale) ### make stage 1x search space
 
+        self.SAModule1x = SAModule1x(self.num_res_blocks[1], n_feats, res_scale)
         ### subpixel 1 -> 2 - PS Module: Upscaling 1x res to 2x res [necessary]
         self.ps12 = UpScaleModule(n_feats)
 
         # Soft Attention Module 2x -> see forward() for actual computation
-        self.stage2x = self.__make_stage2x(num_res_blocks, n_feats, res_scale) ### make stage 1x 2x search space
+        self.stage2x = self.__make_stage(2) ### make stage 1x 2x search space
 
         ### subpixel 2 -> 3 - PS Module: Upscaling 2x res to 4x res [necessary]
         self.ps23 = UpScaleModule(n_feats)
         
-        self.stage3x = self.__make_stage3x(num_res_blocks, n_feats, res_scale) ### make stage 1x 2x 3x search space
+        self.stage3x = self.__make_stage(3) ### make stage 1x 2x 3x search space
 
         # Final Merge stage for 1x 2x 4x feats
         self.merge_tail = MergeTail(n_feats)
     
-    def __make_stage1x(self, num_res_blocks, n_feats, res_scale):
+    def __make_stage(self, n_feats, res_scale, stage_num):
         modules = nn.ModuleList()
-        modules.append(SAModule1x(num_res_blocks, n_feats, res_scale))
+        if stage_num == 2:
+            for _ in range(self.list_modules[0]):
+                modules.append(MixModule(self.num_res_blocks[2], n_feats, res_scale, stage_num))
+        if stage_num == 3:
+            for _ in range(self.list_modules[1]):
+                modules.append(MixModule(self.num_res_blocks[3], n_feats, res_scale, stage_num))
         return modules
     
-    def __make_stage2x(self, num_res_blocks, n_feats, res_scale):
-        modules = nn.ModuleList()
-        modules.append(SAModule2x(num_res_blocks, n_feats, res_scale))
-        modules.append(CSFI2Module(num_res_blocks, n_feats, res_scale))
-        return modules
+    #def __make_stage1x(self, n_feats, res_scale):
+    #    modules = nn.ModuleList()
+    #    modules.append(SAModule1x(self.num_res_blocks, n_feats, res_scale))
+    #    return modules
     
-    def __make_stage3x(self, num_res_blocks, n_feats, res_scale):
-        modules = nn.ModuleList()
-        modules.append(SAModule3x(num_res_blocks, n_feats, res_scale))
-        modules.append(CSFI3Module(num_res_blocks, n_feats, res_scale))
-        return modules
+    #def __make_stage2x(self, n_feats, res_scale):
+    #    modules = nn.ModuleList()
+    #    modules.append(SAModule2x(self.num_res_blocks, n_feats, res_scale))
+    #    modules.append(CSFI2Module(self.num_res_blocks, n_feats, res_scale))
+    #    return modules
+    
+    #def __make_stage3x(self, n_feats, res_scale):
+    #    modules = nn.ModuleList()
+    #    modules.append(SAModule3x(self.num_res_blocks, n_feats, res_scale))
+    #    modules.append(CSFI3Module(self.num_res_blocks, n_feats, res_scale))
+    #    return modules
 
 
-    def forward(self, num_res_blocks, x, S=None, T_lv3=None, T_lv2=None, T_lv1=None):
+    def forward(self, x, S=None, T_lv3=None, T_lv2=None, T_lv1=None):
         ### shallow feature extraction (necessary)
         x = self.SFE(x)
         ### stage1x[0]: SAModule1x
-        x11 = self.stage1x[0](num_res_blocks, x, T_lv3, S)
-
-        x21 = x11
-        x21_res = x21
+        x11, x21, x21_res= self.SAModule1x(x, T_lv3, S)
         ### Upscale from 1x -> 2x
         x22 = self.ps12(x11)
         ### stage2x[0]: SAModule2x
         ### stage2x[1]: CSFI2Module
-        x22 = self.stage2x[0](num_res_blocks, x22, T_lv2, S)
+        x22 = self.stage2x[0](x22, T_lv2, S)
         x21, x22 = self.stage2x[1](x22, x21, x21_res)
 
         x31 = x21
@@ -359,7 +398,7 @@ class MainNet_NAS(nn.Module):
         x33 = self.ps23(x22)
         ### stage3x[0]: SAModule3x
         ### stage3x[1]: CSFI3Module
-        x33 = self.stage3x[0](num_res_blocks, x33, T_lv1, S)
+        x33 = self.stage3x[0](x33, T_lv1, S)
         x31, x32, x33 = self.stage3x[1](x33, x31_res, x32_res, x31, x32)
        
         ## 4-7 ~ 4-10
